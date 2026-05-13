fc25202059832f918f1c055619b0397ee9bafc9d
Phạm Công Định <dinhp5432@gmail.com>
2026-05-14 02:57:21 +0700

    add taildwind package

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+
+// static
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
+
 //http logger
 app.use(morgan("combined"));
 
